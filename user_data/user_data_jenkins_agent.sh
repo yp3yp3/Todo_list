08629a919219e97db3da30783b3f8728b179ac17
yp3yp3 <132269842+yp3yp3@users.noreply.github.com>
2025-04-20 22:13:36 +0300

    Update user_data_jenkins_agent.sh

--- user_data/user_data_jenkins_agent.sh
+++ user_data/user_data_jenkins_agent.sh
@@ -20,9 +20,6 @@ apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker
 # Add 'ubuntu' user to docker group
 usermod -aG docker ubuntu
 
-# Apply group change immediately (no reboot)
-echo 'newgrp docker' >> /home/ubuntu/.bashrc
-
 # Install Python 3.12 and venv
 apt install -y python3.12-venv
 
